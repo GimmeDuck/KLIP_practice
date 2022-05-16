6f64228aa26d6193ff640715fa66111881a5f0f3
gyu-bin-yoon <gyubinyoon99@gmail.com>
2022-05-16 23:53:27 +0900

    테스트넷 위한 caver

---
+++ src/execute.js
@@ -0,0 +1,28 @@
+const Caver = require('caver-js');
+const caver = new Caver('https://api.baobab.klaytn.net:8651/');
+let account = caver.klay.accounts.createWithAccountKey('0x0x38596eD0dceaC58632bCf8BD92B5af3854d6A768', '0x0xf1d92ecda4d008025f094823827513dfd167eb9283b2f08be81bd06f9f2ac1e3')
+caver.klay.accounts.wallet.add(account)
+
+var myContract = new caver.klay.Contract([
+    {
+        constant: false,
+        inputs: [
+        {
+        internalType: "uint256",
+        name: "requestedCount",
+        type: "uint256"
+        }
+        ],
+        name: "publicMint",
+        outputs: [],
+        payable: true,
+        stateMutability: "payable",
+        type: "function"
+    },
+]);
+
+myContract.options.address = '0x226d6A83e725651B48020f6A645D88c7B37005de';
+myContract.options.gas = 5000000;
+myContract.options.gasPrice = '25000000000000';
+myContract.methods.publicMint(1).call().then(console.log);
+
